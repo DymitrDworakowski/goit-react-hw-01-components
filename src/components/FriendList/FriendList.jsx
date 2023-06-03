54817dac79b666de8313e99ecfa4a8cdbb07ba1f
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-06-03 14:44:18 +0200

    friends

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
+import FriendListItem from "./FriendListItem";
 
 const FriendList = ({ friends }) => {
 return (
     <ul className="friend-list">
     {friends.map((friend) => (
-        <li className="item" key={friend.id}>
-        <span className={friend.isOnline ? "status online" : "status offline"}></span>
-        <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
-        <p className="name">{friend.name}</p>
-        </li>
+        <FriendListItem
+        key={friend.id}
+        avatar={friend.avatar}
+        name={friend.name}
+        isOnline={friend.isOnline}
+        />
     ))}
     </ul>
 );
@@ -18,9 +20,9 @@ FriendList.propTypes = {
 friends: PropTypes.arrayOf(
     PropTypes.shape({
     id: PropTypes.number.isRequired,
-    isOnline: PropTypes.bool.isRequired,
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
     })
 ).isRequired,
 };
