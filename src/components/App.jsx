dd162aeca24808edb20fbe0c9250cc6535e0695d
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-06-03 13:12:40 +0200

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import user from './Profile/user.json';
 import Profile from "./Profile/Profile";
 
+import data from './Statistics/data.json';
+import Statistics from './Statistics/Statistics';
+
 export const App = () => {
   return (
     <div>
@@ -11,7 +14,7 @@ export const App = () => {
   avatar={user.avatar}
   stats={user.stats}
       />
-      <h1>Test</h1>
+      <Statistics title="Upload stats" stats={data} />
       </div>
   );
 };
